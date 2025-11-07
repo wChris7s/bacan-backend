5892e6f7fac76e529ca3b64307941e95b424b01d
Christian W. Aranibar Solaligue <cw.aranibar.solaligue@gmail.com>
2025-11-07 01:25:10 -0500

    minor fixes

--- microservices/store/src/main/java/com/bacan/app/infrastructure/adapter/out/persistence/repository/ProductRepository.java
+++ microservices/store/src/main/java/com/bacan/app/infrastructure/adapter/out/persistence/repository/ProductRepository.java
@@ -17,9 +17,9 @@ public interface ProductRepository extends
     SELECT p FROM store.product p
     INNER JOIN store.product_category pc on p.id = pc.product_id
     WHERE (
-        (:#{[0]}.name IS NULL OR lower(p.name) LIKE concat('%', lower(:#{[0]}.name), '%')) AND
-        (:#{[0]}.storeId IS NULL OR p.store_id = :#{[0]}.storeId) AND
-        (:#{[0]}.categoryIds.empty() IS TRUE OR pc.category_id IN (:#{[0]}.categoryIds))
+        (:#{[0].name} IS NULL OR lower(p.name) LIKE concat('%', lower(:#{[0].name}), '%')) AND
+        (:#{[0].storeId} IS NULL OR p.store_id = :#{[0].storeId}) AND
+        (:#{[0].categoryIds.empty()} IS TRUE OR pc.category_id IN (:#{[0].categoryIds}))
     )
     """)
   Flux<ProductEntity> findAllByQuery(ProductQuery query, Pageable pageable);
@@ -28,9 +28,9 @@ public interface ProductRepository extends
     SELECT count(p.id) FROM store.product p
     INNER JOIN store.product_category pc on p.id = pc.product_id
     WHERE (
-        (:#{[0]}.name IS NULL OR lower(p.name) LIKE concat('%', lower(:#{[0]}.name), '%')) AND
-        (:#{[0]}.storeId IS NULL OR p.store_id = :#{[0]}.storeId) AND
-        (:#{[0]}.categoryIds.empty() IS TRUE OR pc.category_id IN (:#{[0]}.categoryIds))
+        (:#{[0].name} IS NULL OR lower(p.name) LIKE concat('%', lower(:#{[0].name}), '%')) AND
+        (:#{[0].storeId} IS NULL OR p.store_id = :#{[0]}.storeId) AND
+        (:#{[0].categoryIds.empty()} IS TRUE OR pc.category_id IN (:#{[0].categoryIds}))
     )
     """)
   Mono<Long> countAllByQuery(ProductQuery query);
